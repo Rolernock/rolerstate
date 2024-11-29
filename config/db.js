29650053f868d2117da9f178d447eb0841fa9a6f
Rolernock <rolernockgoines@gmail.com>
2024-11-29 20:58:43 +0300

    changed DB from logal to remote

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const ConnectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_COMPASS)
+    const conn = await mongoose.connect(process.env.MONGO_ATLAS)
     console.log(`DB connected on: ${conn.connection.host}`)
   } catch (err) {
     console.log(err.message)
